5876a8d8139a7eff45635f5a19d7f174f0e8f1e8
Mahmoud Ali <mahmoudalifarag1@gmail.com>
2022-08-24 15:13:56 +0200

    Add a new update

--- main.js
+++ main.js
@@ -1,105 +1,187 @@
-let price = document.querySelector(".price-input");
-let addBtn = document.querySelector("button");
+let priceInputForNewValue = document.querySelector(".price-input");
+let addNewPriceBtn = document.querySelector("#add-new-value");
 let selectClient = document.querySelector(".select-client");
-let option1 = document.querySelectorAll(".select-client option")[1];
-let option2 = document.querySelectorAll(".select-client  option")[2];
-let clientOne = document.querySelector(".client-one .client-income span");
-let clientTwo = document.querySelector(".client-Two .client-income span");
 let invalidMsg = document.querySelector(".invalid-msg");
 let totalPrice = document.querySelector(".total-price span");
-let clientOneName = document.querySelector(".client-one .client-name span");
-let clientTwoName = document.querySelector(".client-two .client-name span");
+
 let showEditNameBtn = document.querySelectorAll(".client-name i");
 let editNameParent = document.querySelector(".change-name");
 let editNameValue = document.querySelector(".change-name input");
 let editeNameBtn = document.querySelector(".change-name button");
 let overlay = document.querySelector(".overlay");
 let exitChangeName = document.querySelector(".change-name span");
+let clientsParent = document.querySelector(".clients");
+let addNewClient = document.querySelector(".add-new-client");
 
-let priceOne = window.localStorage.getItem("price-one");
-let priceTwo = window.localStorage.getItem("price-two");
-let totalPriceFromLocal = window.localStorage.getItem("total-price");
-let clientOneNameFromLocal = window.localStorage.getItem("client-one-name");
-let clientTwoNameFromLocal = window.localStorage.getItem("client-two-name");
+let arrayOfClients = [];
+let arrayOfPaymentData = [];
 
-addBtn.onclick = function () {
-  if (price.value != "") {
-    if (selectClient.options[selectClient.selectedIndex].id == "1") {
-      if (clientOne.textContent == 0) {
-        clientOne.textContent = price.value;
-      } else {
-        clientOne.textContent =
-          Number(clientOne.textContent) + Number(price.value);
-      }
+if (localStorage.getItem("total-price") != null) {
+  totalPrice.innerHTML = localStorage.getItem("total-price");
+}
 
-      window.localStorage.setItem("price-one", clientOne.textContent);
-      invalidMsg.classList.remove("show");
-      price.value = "";
-      calcTotal();
-    } else if (selectClient.options[selectClient.selectedIndex].id == "2") {
-      if (clientTwo.textContent == 0) {
-        clientTwo.textContent = price.value;
-      } else {
-        clientTwo.textContent =
-          Number(clientTwo.textContent) + Number(price.value);
-      }
-      window.localStorage.setItem("price-two", clientTwo.textContent);
+if (localStorage.getItem("payment-details") !== null) {
+  arrayOfPaymentData = JSON.parse(localStorage.getItem("payment-details"));
+}
+if (localStorage.getItem("clients") != null) {
+  arrayOfClients = JSON.parse(localStorage.getItem("clients"));
+}
 
-      invalidMsg.classList.remove("show");
-      price.value = "";
-      calcTotal();
-    } else {
-      invalidMsg.textContent = "Please Select Your Client";
-      invalidMsg.classList.add("show");
-    }
-  } else {
-    invalidMsg.textContent = "Please Enter Income";
-    invalidMsg.classList.add("show");
+function addTaskToArray(clientName, classPayment) {
+  // client Data
+  const clients = {
+    id: Date.now(),
+    name: clientName,
+    price: classPayment,
+    completed: false,
+  };
+  // Push Tasks To Array Of Task
+  arrayOfClients.push(clients);
+  addDataToLocalStorageFrom(arrayOfClients);
+}
+
+// Step 1
+
+function paymentDetailsDataTo(arrayOfPaymentData) {
+  window.localStorage.setItem(
+    "payment-details",
+    JSON.stringify(arrayOfPaymentData)
+  );
+}
+
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+// Step 2
+
+function addPaymentDetailsToArray(clientName, classPayment) {
+  let dateNow = new Date();
+
+  let paymentDate = dateNow.getDate() + " " + months[dateNow.getMonth()];
+  // client Data
+  const details = {
+    date: paymentDate,
+    name: clientName,
+    price: classPayment,
+  };
+  // Push Tasks To Array Of Task
+  arrayOfPaymentData.push(details);
+  paymentDetailsDataTo(arrayOfPaymentData);
+}
+
+function clearDataFromInput() {
+  priceInputForNewValue.value = "";
+}
+addNewClient.onclick = function () {
+  editNameParent.classList.toggle("show");
+  overlay.classList.toggle("show");
+  editeNameBtn.innerHTML = "Add New Client";
+  editeNameBtn.style.backgroundColor = "#007bff";
+};
+
+editeNameBtn.onclick = function () {
+  if (editNameValue.value != "") {
+    addTaskToArray(editNameValue.value, 0);
+    editNameParent.classList.remove("show");
+    overlay.classList.remove("show");
   }
 };
 
-function calcTotal() {
-  totalPrice.textContent =
-    Number(clientOne.textContent) + Number(clientTwo.textContent);
-  window.localStorage.setItem("total-price", totalPrice.textContent);
+function addDataToLocalStorageFrom(arrayOfClients) {
+  window.localStorage.setItem("clients", JSON.stringify(arrayOfClients));
 }
 
-if (priceTwo != null && priceOne != null && totalPriceFromLocal != null) {
-  clientOne.textContent = priceOne;
-  clientTwo.textContent = priceTwo;
-  totalPrice.textContent = totalPriceFromLocal;
-}
+function clientsDataFromLocalStorage() {
+  arrayOfClients.forEach((el, i) => {
+    let div = document.createElement("div");
+    let h2 = document.createElement("h2");
+    let inCome = document.createElement("div");
+    let clientPayment = document.createTextNode(arrayOfClients[i].price);
+    let clientName = document.createTextNode(arrayOfClients[i].name);
+    let clientOption = document.createElement("option");
+    let clientOptionText = document.createTextNode(arrayOfClients[i].name);
+    let spanIncome = document.createElement("span");
+    let spanIncomeText = document.createTextNode("Total : ");
 
-showEditNameBtn.forEach((el) => {
-  el.addEventListener("click", function (e) {
-    editNameParent.classList.toggle("show");
-    overlay.classList.toggle("show");
-    console.log(e.target.parentElement.getAttribute("id"));
-    editeNameBtn.onclick = function () {
-      if (editNameValue.value != "") {
-        if (e.target.parentElement.getAttribute("id") == 1) {
-          window.localStorage.setItem("client-one-name", editNameValue.value);
-          clientOneName.textContent = editNameValue.value;
-        } else {
-          window.localStorage.setItem("client-two-name", editNameValue.value);
-          clientTwoName.textContent = editNameValue.value;
+    inCome.className = "client-income";
+    div.setAttribute("id", arrayOfClients[i].id);
+    clientOption.setAttribute("id", arrayOfClients[i].id);
+    div.className = "client-one";
+    h2.className = "client-name";
+
+    spanIncome.appendChild(spanIncomeText);
+    clientOption.appendChild(clientOptionText);
+    selectClient.appendChild(clientOption);
+    clientsParent.appendChild(div);
+    div.appendChild(h2);
+    div.appendChild(inCome);
+
+    h2.appendChild(clientName);
+    inCome.appendChild(spanIncome);
+    inCome.appendChild(clientPayment);
+    addNewPriceBtn.addEventListener("click", function (e) {
+      if (
+        priceInputForNewValue.value != "" &&
+        selectClient.options[selectClient.selectedIndex].text != "Select"
+      ) {
+        invalidMsg.classList.remove("show");
+
+        let resultFromInput =
+          Number(clientPayment.nodeValue) + Number(priceInputForNewValue.value);
+        if (div.id == selectClient.options[selectClient.selectedIndex].id) {
+          totalPrice.innerHTML =
+            Number(totalPrice.innerHTML) + Number(priceInputForNewValue.value);
+          localStorage.setItem("total-price", totalPrice.innerHTML);
+
+          if (Number(clientPayment.nodeValue) !== 0) {
+            arrayOfClients[i].price = resultFromInput;
+            clientPayment.nodeValue = resultFromInput;
+
+            addDataToLocalStorageFrom(arrayOfClients);
+
+            addPaymentDetailsToArray(
+              selectClient.options[selectClient.selectedIndex].text,
+              priceInputForNewValue.value
+            );
+
+            paymentDetailsDataTo(arrayOfPaymentData);
+          } else {
+            arrayOfClients[i].price = priceInputForNewValue.value;
+            clientPayment.nodeValue = priceInputForNewValue.value;
+
+            addDataToLocalStorageFrom(arrayOfClients);
+
+            paymentDetailsDataTo(arrayOfPaymentData);
+            addPaymentDetailsToArray(
+              selectClient.options[selectClient.selectedIndex].text,
+              priceInputForNewValue.value
+            );
+          }
+          clearDataFromInput();
         }
-        editNameParent.classList.toggle("show");
-        overlay.classList.toggle("show");
-        editNameValue.value = "";
+      } else {
+        invalidMsg.innerHTML = "Please Enter Price Or Select Your Client";
+        invalidMsg.classList.add("show");
       }
-    };
+    });
   });
-});
-
-exitChangeName.onclick = function () {
-  editNameParent.classList.remove("show");
-  overlay.classList.remove("show");
-};
-
-if (clientOneNameFromLocal != null && clientTwoNameFromLocal != null) {
-  clientOneName.textContent = clientOneNameFromLocal;
-  clientTwoName.textContent = clientTwoNameFromLocal;
-  option1.textContent = clientOneNameFromLocal;
-  option2.textContent = clientTwoNameFromLocal;
+  localStorage.setItem("total-price", totalPrice.innerHTML);
 }
+
+if (localStorage.getItem("total-price") != null) {
+  totalPrice.innerHTML = localStorage.getItem("total-price");
+}
+
+clientsDataFromLocalStorage();
---
+++ priceDetails.js
@@ -0,0 +1,45 @@
+let detailsParent = document.querySelector(".details-parent");
+let totalPrice = document.querySelector(".total-price span");
+let spendInput = document.querySelector(".spend");
+let spendBtn = document.querySelector(".neg-spend");
+
+let paymentData = JSON.parse(localStorage.getItem("payment-details"));
+
+if (localStorage.getItem("total-price") != null) {
+  totalPrice.innerHTML = localStorage.getItem("total-price");
+}
+
+paymentData.forEach((e, i) => {
+  let paymentParent = document.createElement("div");
+  let dateSpan = document.createElement("span");
+  let nameSpan = document.createElement("span");
+  let priceSpan = document.createElement("span");
+
+  let dateText = document.createTextNode(paymentData[i].date);
+  let nameText = document.createTextNode(paymentData[i].name);
+  let priceText = document.createTextNode(paymentData[i].price);
+
+  paymentParent.className = "details";
+  nameSpan.style.width = "90px";
+
+  dateSpan.appendChild(dateText);
+  nameSpan.appendChild(nameText);
+  priceSpan.appendChild(priceText);
+
+  paymentParent.appendChild(nameSpan);
+  paymentParent.appendChild(priceSpan);
+  paymentParent.appendChild(dateSpan);
+
+  detailsParent.appendChild(paymentParent);
+});
+
+spendBtn.onclick = function () {
+  if (spendInput.value != "") {
+    totalPrice.innerHTML =
+      Number(totalPrice.innerHTML) - Number(spendInput.value);
+
+    localStorage.setItem("total-price", totalPrice.innerHTML);
+
+    spendInput.value = "";
+  }
+};
